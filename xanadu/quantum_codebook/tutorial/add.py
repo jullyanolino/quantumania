0411db177365c6c25c59a144103995f754f3715d
Jullyano Lino <jullyanolino@gmail.com>
2023-03-25 01:16:31 -0300

    Create add.py

---
+++ xanadu/quantum_codebook/tutorial/add.py
@@ -0,0 +1,21 @@
+"""
+Codercise T.1. Complete the function in the code cell below to add together two numbers. Press the "Submit" button to send your work to the grader.
+"""
+
+
+def add_numbers(x, y):
+    """A simple function to add two numbers.
+    
+    Args:
+        x (float): a number
+        y (float): another number
+          
+    Returns:
+        float: The sum of x and y.
+    """
+
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    return x + y
